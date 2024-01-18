07ad4a9a8b52c43115ab8686482acef7de0ab288
Rafael Silva <nightmarezlolzin@gmail.com>
2024-01-17 21:06:07 -0300

    Adição das funcionalidades de postagens CRUD

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+//= link base/typography.css
+//= link layout/posts_layouts.css
+//= link layout/home_layout.css
