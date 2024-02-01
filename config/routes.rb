438b6d50ae254e0b0cf980fe354ca26b8f925d5a
Rafazg <nightmarezlolzin@gmail.com>
2024-02-01 12:19:29 -0300

    Adição de configurações iniciais de autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :posts
 
+  devise_for :users
+  
   get 'about', to: 'pages#about'
 
   get "up" => "rails/health#show", as: :rails_health_check
 
   get 'tags/:id/posts', to: 'tags#posts', as: :tag_posts
 
-  root "pages#home"
+
+  root to: 'pages#home'
+  
 end
