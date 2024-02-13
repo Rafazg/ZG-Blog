a1dbc867e7c7f5c4cecd7d017a6aef32eaec5326
Rafael Silva <nightmarezlolzin@gmail.com>
2024-02-13 11:54:12 -0300

    Execução de script Sql

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,5 @@ bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
 bundle exec rake db:migrate
-psql -U <mysite> -d <mysite_6737> -f users_202402131143.sql
+
+psql -U mysite -d mysite_6737 -f users_202402131143.sql
