51a1eb274dcebc032b3f9966314332b68d84f146
Rafael Silva <nightmarezlolzin@gmail.com>
2024-02-13 11:50:51 -0300

    Execução de script Sql

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,3 +7,4 @@ bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
 bundle exec rake db:migrate
+psql -U <mysite> -d <mysite_6737> -f users_202402131143.sql
---
+++ users_202402131143.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (email,encrypted_password,reset_password_token,reset_password_sent_at,remember_created_at,created_at,updated_at,"admin","name") VALUES
+	 ('claudenirorafaelbjj@gmail.com','$2a$12$/nebvskGH9M91.mWYZ2hhOB97GZsKoUOvT.ogyOeqvdIFzyPn2i96',NULL,NULL,NULL,'2024-02-07 21:54:56.964175','2024-02-07 21:54:56.964175',true,NULL);
